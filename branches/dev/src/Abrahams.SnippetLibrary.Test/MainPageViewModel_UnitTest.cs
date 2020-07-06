16969240713c53e7725a8037495f4576c2908d69
Kian <Kian@9354423a-21f5-9b44-98c6-5ac6c59264a1>
2020-07-06 14:10:37 +0000

    Setup nunit tests
    git-svn-id: https://desktop-k63b8r8/svn/abrahams@15 9354423a-21f5-9b44-98c6-5ac6c59264a1

---
+++ branches/dev/src/Abrahams.SnippetLibrary.Test/MainPageViewModel_UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Abrahams.SnippetLibrary.Test
+{
+    [TestFixture]
+    public class MainPageViewModel_UnitTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
