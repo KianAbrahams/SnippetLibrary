ae9dfc9b50e5f5e9e6e6501d9d51d586bbb27b3a
Kian <Kian@9354423a-21f5-9b44-98c6-5ac6c59264a1>
2020-07-13 13:41:57 +0000

    switched to read only tags collection.
    git-svn-id: https://desktop-k63b8r8/svn/abrahams@38 9354423a-21f5-9b44-98c6-5ac6c59264a1

--- branches/dev/src/Abrahams.SnippetLibrary/DomainModel/CodeSnippet.cs
+++ branches/dev/src/Abrahams.SnippetLibrary/DomainModel/CodeSnippet.cs
@@ -7,8 +7,7 @@ namespace Abrahams.SnippetLibrary.DomainModel
         public string Description { get; set; }
         public string CodeSample { get; set; }
         public Language Language { get; set; }
-        public List<Tag> Tags { get; set; } = new List<Tag>();
-        // TODO: Switch to nullable reference types c# 8 feature
-        // TODO: switch to readonly property with behaviour methods 
+        public List<Tag> Tags { get; } = new List<Tag>();
+        // TODO: Switch to nullable reference types c# 8 features
     }
 }
