a5c5ce58707cb6b09d694a3b30c10d36a83719c6
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2020-07-17 15:35:27 +0100

    WIP added SQL database project and tables to the solution.

---
+++ src/Abrahams.SnippetLibrary.Database/dbo/Tables/CodeSnippetTag.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[CodeSnippetTags]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ src/Abrahams.SnippetLibrary.Database/dbo/Tables/Language.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE dbo.[Language]
+(
+	LanguageId UNIQUEIDENTIFIER NOT NULL,
+	LanguageName NVARCHAR(30) NOT NULL,
+	CONSTRAINT PK_Language PRIMARY KEY (LanguageId),
+	CONSTRAINT UQ_Language_LanguageName UNIQUE(LanguageName)
+)
