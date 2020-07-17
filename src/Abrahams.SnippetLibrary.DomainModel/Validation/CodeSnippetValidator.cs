fe9000dfa6769a4dac255bc9f49ecbe2ab8f9e05
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2020-07-17 15:36:13 +0100

    Add TODOs to validations

--- src/Abrahams.SnippetLibrary.DomainModel/Validation/CodeSnippetValidator.cs
+++ src/Abrahams.SnippetLibrary.DomainModel/Validation/CodeSnippetValidator.cs
@@ -6,7 +6,9 @@ namespace Abrahams.SnippetLibrary.DomainModel.Validation
     {
         public CodeSnippetValidator(ILanguageValidator languageValidator, ITagValidator tagValidator)
         {
+            // TODO: add validation logic for the largest size for description (255 characters).
             this.RuleFor(x => x.Description).NotEmpty().WithMessage(RequiredErrorMessage);
+            // TODO: decide if we want to add validation logic for the largest size for Code sample (2 GBs).
             this.RuleFor(x => x.CodeSample).NotEmpty().WithMessage(RequiredErrorMessage);
             this.RuleFor(x => x.Language).NotNull().WithMessage("Please select a '{PropertyName}'.");
             this.RuleFor(x => x.Language).SetValidator(languageValidator).When(x => x.Language != null, ApplyConditionTo.CurrentValidator);
