9b215cc4513418d328e60f23f71d2b8518df6bb9
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2020-07-17 15:34:33 +0100

    Adding DAL implimentation for SqlClient

---
+++ src/Abrahams.SnippetLibrary.DAL/SqlClient/SqlClientLanguageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Abrahams.SnippetLibrary.DomainModel;
+using System.Collections.Generic;
+
+namespace Abrahams.SnippetLibrary.DAL.SqlClient
+{
+    public class SqlClientLanguageRepository : ILanguageRepository
+    {
+        public List<Language> GetLanguageList()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
