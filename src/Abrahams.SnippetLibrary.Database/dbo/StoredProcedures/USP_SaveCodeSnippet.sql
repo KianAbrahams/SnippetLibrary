a2c8352125da586a531620a46c7d05cad9d5a147
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2020-07-22 16:13:10 +0100

    added USP_SaveCodeSnippet

--- src/Abrahams.SnippetLibrary.Database/dbo/StoredProcedures/USP_SaveCodeSnippet.sql
+++ src/Abrahams.SnippetLibrary.Database/dbo/StoredProcedures/USP_SaveCodeSnippet.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE dbo.USP_SaveCodeSnippet 
 (
 	@Description NVARCHAR(255), 
+	@CodeSample NVARCHAR(MAX),
 	@LanguageId INT, 
 	@CodeSnippetId INT = 0
 )
@@ -9,13 +10,14 @@ AS
 	BEGIN
 		UPDATE dbo.CodeSnippet
 		SET [Description] = @Description, 
-			LanguageId = @LanguageId
+			LanguageId = @LanguageId,
+			CodeSample = @CodeSample
 		WHERE CodeSnippetId = @CodeSnippetId
 	END 
 	ELSE
 	BEGIN
-		INSERT INTO dbo.CodeSnippet ([Description], LanguageId) 
-		VALUES (@Description, @LanguageId)
+		INSERT INTO dbo.CodeSnippet ([Description], CodeSample, LanguageId) 
+		VALUES (@Description, @CodeSample, @LanguageId)
 	END
 
 RETURN 0
