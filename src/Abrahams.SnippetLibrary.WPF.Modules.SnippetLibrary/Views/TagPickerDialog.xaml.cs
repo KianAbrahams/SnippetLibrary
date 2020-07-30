08adede11596ed71ad7cdfecf1656bc20ed1bd29
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2020-07-30 17:24:31 +0100

    WIP AddTagDialog

---
+++ src/Abrahams.SnippetLibrary.WPF.Modules.SnippetLibrary/Views/TagPickerDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Abrahams.SnippetLibrary.Modules.SnippetLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abrahams.SnippetLibrary.Modules.SnippetLibrary.Views
+{
+    /// <summary>
+    /// Interaction logic for AddTagPage.xaml
+    /// </summary>
+    public partial class TagPickerDialog : Window
+    {
+        public TagPickerDialog(IAddTagPageViewModel addTagPageViewModel)
+        {
+            this.InitializeComponent();
+
+            this.DataContext = addTagPageViewModel;
+        }
+    }
+}
---
+++ src/Abrahams.SnippetLibrary/Modules/SnippetLibrary/ViewModels/IAddTagPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Abrahams.SnippetLibrary.Modules.SnippetLibrary.ViewModels
+{
+    public interface IAddTagPageViewModel
+    {
+         event EventHandler CloseDialog;
+
+         ICommand Cancel { get; }
+    }
+}
